7bdf65804777f4d9aba7843097dbd9c1b61bd028
Brian Fontana <brian@codefoxtrot.com>
2023-08-29 21:33:00 -0400

    housekeeping

--- BlazorSimpleAuth/Program.cs
+++ BlazorSimpleAuth/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
